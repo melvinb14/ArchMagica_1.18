a8abd15a03cad1651e8fd4363ba2a5d0c4cfbae5
Melvin <65771068+melvinb14@users.noreply.github.com>
2022-01-19 17:05:47 +0100

    Initial commit

---
+++ src/main/java/com/melvinbur/archmagica/ArchMagica.java
@@ -0,0 +1,56 @@
+package com.melvinbur.archmagica;
+
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+@Mod(ArchMagica.MOD_ID)
+public class ArchMagica {
+    public static final String MOD_ID = "archmagica";
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+
+    public ArchMagica() {
+
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        eventBus.addListener(this::setup);
+        eventBus.addListener(this::setupClient);
+
+
+
+
+
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setupClient(final FMLClientSetupEvent event) {
+
+    }
+    private void setup(final FMLCommonSetupEvent event) {
+
+
+
+
+
+    }
+}
